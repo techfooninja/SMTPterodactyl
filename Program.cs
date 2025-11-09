a3eb3b23736fee29f16436ad7dc1c68fe9ebb012
techfooninja <josh@davis.fm>
2025-11-08 22:46:16 -0600

    check in working prototype of email to Telegram bot

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using SmtpServer;
+using SmtpServer.ComponentModel;
+using SMTPterodactyl;
+
+Console.WriteLine($"Args: {string.Join(", ", args)}");
+
+var options = new SmtpServerOptionsBuilder()
+    .ServerName("localhost")
+    .Port(25, 587)
+    .Build();
+
+var serviceProvider = new ServiceProvider();
+serviceProvider.Add(new TestMessageStore(args[0], long.Parse(args[1])));
+var smtpServer = new SmtpServer.SmtpServer(options, serviceProvider);
+Console.WriteLine("Starting SMTP Server...\r\n");
+await smtpServer.StartAsync(CancellationToken.None);
+Console.WriteLine("Ending SMTP Server");
+Console.ReadLine();
---
+++ TestMessageStore.cs
@@ -0,0 +1,48 @@
+﻿namespace SMTPterodactyl
+{
+    using MimeKit;
+    using SmtpServer;
+    using SmtpServer.Protocol;
+    using SmtpServer.Storage;
+    using System.Buffers;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Telegram.Bot;
+
+    internal class TestMessageStore : IMessageStore
+    {
+        private readonly string token;
+        private readonly long chatId;
+        private ITelegramBotClient client;
+
+        public TestMessageStore(string telegramToken, long telegramChatId)
+        {
+            this.token = telegramToken;
+            this.chatId = telegramChatId;
+            this.client = new TelegramBotClient(this.token);
+
+            // TODO: Add logic to reply with the current chatId
+        }
+
+        public async Task<SmtpResponse> SaveAsync(ISessionContext context, IMessageTransaction transaction, ReadOnlySequence<byte> buffer, CancellationToken cancellationToken)
+        {
+            await using var stream = new MemoryStream();
+
+            var position = buffer.GetPosition(0);
+            while (buffer.TryGet(ref position, out var memory))
+            {
+                await stream.WriteAsync(memory, cancellationToken);
+            }
+
+            stream.Position = 0;
+            var message = await MimeMessage.LoadAsync(stream, cancellationToken);
+            Console.WriteLine($"To: {message.To}\r\nFrom: {message.From}\r\nSubject: {message.Subject}\r\nBody: {message.TextBody}\r\n");
+
+            await message.WriteToAsync(Path.Combine(@"C:\temp", $"{Guid.NewGuid()}.msg"));
+
+            await this.client.SendMessage(new Telegram.Bot.Types.ChatId(this.chatId), $"{message.Subject}\r\n\r\n{message.TextBody}");
+
+            return SmtpResponse.Ok;
+        }
+    }
+}
