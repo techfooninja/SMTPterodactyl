222d8f1d33768bc11f1996360afa553b161be6d6
techfooninja <josh@davis.fm>
2026-07-10 02:41:21 -0500

    fixed some warnings

--- SMTPterodactyl/SmtpOptions.cs
+++ SMTPterodactyl/SmtpOptions.cs
@@ -2,7 +2,7 @@
 
 public class SmtpOptions
 {
-    public int[] Ports { get; set; }
+    public int[] Ports { get; set; } = new int[] { 25 };
 
-    public string ServerName { get; set; }
+    public string ServerName { get; set; } = "localhost";
 }
