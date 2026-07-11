80682f91bb2c0a123033a5edc56e65e310537f13
techfooninja <josh@davis.fm>
2026-07-10 23:42:26 -0500

    add publish settings and make it runnable as a windows service

--- SMTPterodactyl/Program.cs
+++ SMTPterodactyl/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using SmtpServer;
@@ -47,6 +48,14 @@ services.AddSingleton(services =>
 });
 
 services.AddScoped<ProcessInboundMessageHandler>();
+
+
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = configuration.GetValue<string>("WindowsServiceName");
+});
+
+
 services.AddHostedService<SmtpHostedService>();
 
 var host = builder.Build();
